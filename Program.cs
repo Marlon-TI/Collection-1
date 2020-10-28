66fd8909a773684e572e176c3172a3ee7ea9c325
Marlon <marlonsmoreira@gmail.com>
2020-10-28 10:13:15 -0300

    Collection

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+            Console.WriteLine(set.Contains("Notebook"));
+            foreach (String p in set)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
